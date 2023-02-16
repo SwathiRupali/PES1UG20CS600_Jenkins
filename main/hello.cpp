59e2e067005b0e4c44db25f50a59b7d7c666dad2
Swathi Rupali NV <nvrupali0@gmail.com>
2023-02-16 09:50:20 +0530

    Changed Cpp File

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,8 +1,9 @@
 //File: hello.cpp
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main(void) {
+int main() {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
